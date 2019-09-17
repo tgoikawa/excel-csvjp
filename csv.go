af3758de79400b4c3fe8e7a0fb8aae952cd4a574
tgoikawa <kosuke.oikawa@topgate.co.jp>
2019-09-17 11:41:47 +0900

    リーダー・ライターを実装

---
+++ csv.go
@@ -0,0 +1,21 @@
+package jpcsv
+
+import (
+	"encoding/csv"
+	"io"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+// NewReader 与えられたio.Readerを元に新しいcsvリーダーを返す
+func NewReader(r io.Reader) *csv.Reader {
+	return csv.NewReader(transform.NewReader(r, japanese.ShiftJIS.NewDecoder()))
+}
+
+// NewWriter 与えられたio.Writerを元に新しいcsvライターを返す
+func NewWriter(w io.Writer) *csv.Writer {
+	writer := csv.NewWriter(transform.NewWriter(w, japanese.ShiftJIS.NewDecoder()))
+	writer.UseCRLF = true
+	return writer
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/tgoikawa/jpcsv
+
+require (
+	github.com/gocarina/gocsv v0.0.0-20190821091544-020a928c6f4e
+	golang.org/x/text v0.3.2
+)
